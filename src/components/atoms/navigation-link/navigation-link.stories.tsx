6e115e05dd485e30928268fa4f80ad1fbd9f898e
Michał Kołacz <michal.kolacz45@gmail.com>
2024-12-27 10:24:10 +0100

    fix: update NavigationLink component to use 'href' prop instead of 'to'

--- src/components/atoms/navigation-link/navigation-link.stories.tsx
+++ src/components/atoms/navigation-link/navigation-link.stories.tsx
@@ -1,40 +1,40 @@
-import { type Meta, type StoryObj } from '@storybook/react'
-import { disableControls, setDesignPreview } from '#app/utils/storybook.ts'
-import { NavigationLink } from './navigation-link.tsx'
+import { type Meta, type StoryObj } from "@storybook/react";
+import { disableControls, setDesignPreview } from "#app/utils/storybook.ts";
+import { NavigationLink } from "./navigation-link.tsx";
 
 const meta = {
-	title: 'Atoms/Navigation Link',
-	component: NavigationLink,
-	parameters: {
-		layout: 'centered',
-		...setDesignPreview(
-			'https://www.figma.com/file/zx5FkmiDiV7F8Jn5phnpeJ/Ence-Pence-Huta-v2?node-id=20-32&t=fp53uoEO5s9CIEdr-4',
-		),
-	},
-	args: { children: 'Strona główna', to: '/abc' },
-	argTypes: {
-		...disableControls<typeof NavigationLink>(['about']),
-	},
-	tags: ['autodocs'],
-} satisfies Meta<typeof NavigationLink>
+  title: "Atoms/Navigation Link",
+  component: NavigationLink,
+  parameters: {
+    layout: "centered",
+    ...setDesignPreview(
+      "https://www.figma.com/file/zx5FkmiDiV7F8Jn5phnpeJ/Ence-Pence-Huta-v2?node-id=20-32&t=fp53uoEO5s9CIEdr-4"
+    ),
+  },
+  args: { children: "Strona główna", href: "/abc" },
+  argTypes: {
+    ...disableControls<typeof NavigationLink>(["about"]),
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof NavigationLink>;
 
-export default meta
-type Story = StoryObj<typeof meta>
+export default meta;
+type Story = StoryObj<typeof meta>;
 
 export const Component: Story = {
-	args: {},
-}
+  args: {},
+};
 
 export const Variants: Story = {
-	render: args => {
-		return (
-			<div className="flex gap-4">
-				<NavigationLink {...args} to="/" />
-				<NavigationLink {...args} to="/abc" />
-			</div>
-		)
-	},
-	argTypes: {
-		...disableControls<typeof NavigationLink>(['to']),
-	},
-}
+  render: (args) => {
+    return (
+      <div className="flex gap-4">
+        <NavigationLink {...args} href="/" />
+        <NavigationLink {...args} href="/abc" />
+      </div>
+    );
+  },
+  argTypes: {
+    ...disableControls<typeof NavigationLink>(["href"]),
+  },
+};
